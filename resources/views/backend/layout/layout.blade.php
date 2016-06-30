8cd4ef4506bbd6d5cf81a8239a885afcc6e49651
phillipmadsen <pmadsen2013@gmail.com>
2016-06-30 15:30:14 -0600

    UPDATE
    AGAIN

--- resources/views/backend/layout/layout.blade.php
+++ resources/views/backend/layout/layout.blade.php
@@ -21,7 +21,7 @@
     <link href="{!! url('backend/css/AdminLTE.min.css') !!}" rel="stylesheet" type="text/css"/>
 
     <link href="{!! url('backend/css/style.css') !!}" rel="stylesheet" type="text/css"/>
-    {!! HTML::style("assets/css/github-left.css") !!}
+
     <!-- jQuery 2.1.3 -->
     <script src="{!! url('backend/plugins/jQuery/jQuery-2.1.3.min.js') !!}"></script>
     <!-- Bootstrap 3.3.2 JS -->
@@ -49,7 +49,7 @@
     <!-- AdminLTE for demo purposes -->
     <script src="{!! url('backend/js/demo.js') !!}" type="text/javascript"></script>
 
-    <!-- AdminLTE Skins. Choose a skin from the css/skins 
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
     <link href="{!! url('backend/css/skins/_all-skins.min.css') !!}" rel="stylesheet" type="text/css"/>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -58,7 +58,7 @@
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script><![endif]-->
 </head>
 <body class="skin-blue">
-<span id="forkongithub"><a target="_blank" href="https://github.com/sseffa/fullycms">Fork me on GitHub</a></span>
+
 <div class="wrapper">
     <header class="main-header">
         <!-- Logo -->
