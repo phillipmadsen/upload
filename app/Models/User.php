020e73ceb2823ebb748e3fb21256d4eb07df09a1
vagrant <vagrant@homestead>
2016-06-30 17:44:39 +0000

    initial files added

---
+++ app/Models/User.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Fully\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Cartalyst\Sentinel\Users\EloquentUser;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\HasMedia\Interfaces\HasMedia;
+
+/**
+ * Class User.
+ *
+ * @author Phillip Madsen <contact@affordableprogrammer.com>
+ */
+class User extends EloquentUser implements HasMedia
+{
+	use HasMediaTrait;
+
+
+
+	public function articles()
+    {
+        return $this->hasMany(\Fully\Models\Article::class, 'user_id')->where('status_id', 3)->orderBy('created_at', 'DESC');
+    }
+    public function all_articles()
+    {
+        return $this->hasMany(\Fully\Models\Article::class, 'user_id')->orderBy('created_at', 'DESC');
+    }
+    public function latest_articles()
+    {
+        return $this->hasMany(\Fully\Models\Article::class, 'user_id')->where('status_id', 3)->orderBy('created_at', 'DESC')->take(5);
+    }
+
+
+	public static function edit($user_id, /*$name, $surname, $username,*/ $email, $first_name, $last_name)
+    {
+        $user = static::find($user_id);
+        $user->first_name = $first_name;
+        $user->last_name = $last_name;
+        // $user->surname = $surname;
+        // $user->username = $username;
+        $user->email = $email;
+        return $user;
+    }
+
+}
